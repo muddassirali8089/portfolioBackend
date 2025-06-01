7712e8ebe6d239f79d8d55261bfbc89de0abed47
muddassirali8089 <114619571+muddassirali8089@users.noreply.github.com>
2025-06-01 15:31:34 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# portfolioBackend
+this is backend for my porfolio
---
+++ portfolioApp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello from portfolioApp!")
+
+
+def contact(request):
+    return HttpResponse("this is contact...!")
+
